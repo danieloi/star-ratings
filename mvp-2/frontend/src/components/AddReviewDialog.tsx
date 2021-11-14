96fbe6ddb8cc9e1356e72d22066911584456568c
Mayowa Daniel <mayowadan17@gmail.com>
2021-11-14 07:13:40 +0100

    improve vertical alignment for dialog

--- mvp-2/frontend/src/components/AddReviewDialog.tsx
+++ mvp-2/frontend/src/components/AddReviewDialog.tsx
@@ -12,7 +12,7 @@ type Props = {
 const cssClasses = {
   overlay:
     "fixed top-0 bottom-0 right-0 left-0 z-9999 bg-black-10 flex justify-center items-center",
-  content: " bg-white br4 measure-wide ph4 pv4 w-100",
+  content: " bg-white br4 measure-wide ph4 pv4 w-100 mb4 mb6-l",
 };
 
 export default function AddReviewDialog({
